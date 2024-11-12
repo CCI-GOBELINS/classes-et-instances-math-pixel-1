a57fd7fcff3c56daf7fddef61e8cb6d9a100f2e6
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-11-12 16:09:19 +0100

    exercise

--- app/src/test/java/com/gmail/eamosse/tp2/LocalStudentManagerTest.kt
+++ app/src/test/java/com/gmail/eamosse/tp2/LocalStudentManagerTest.kt
@@ -14,6 +14,7 @@ class LocalStudentManagerTest {
      */
     @Before
     fun tearUp() {
+        manager = LocalStudentManager();
         // TODO create an instance of the local student manager
     }
 
@@ -31,7 +32,8 @@ class LocalStudentManagerTest {
     fun sortAgeAsc_isCorrect() {
         val result = manager.sortAgeAscDesc("ASC")
         assertEquals(10, result.first().age)
-        assertEquals(16, result.last().age)
+        // HERE, IT SHOULD BE 56, NOT 10
+        assertEquals(56, result.last().age)
     }
 
     @Test
